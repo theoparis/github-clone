dc051f48f6dbbafccc4cab291c088e8206fd6303
Theo Paris <theoparisdesigns@gmail.com>
2024-12-10 14:29:30 -0800

    feat: add a sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,14 @@
   import Header from './lib/components/Header.svelte';
   import RepoNav from './lib/components/RepoNav.svelte';
   import FileExplorer from './lib/components/FileExplorer.svelte';
+  import Sidebar from './lib/components/Sidebar.svelte';
 </script>
 
-<main class="min-h-screen">
-  <Header />
-  <RepoNav />
-  <FileExplorer />
+<main class="min-h-screen flex">
+  <div class="flex-grow">
+    <Header />
+    <RepoNav />
+    <FileExplorer />
+  </div>
+  <Sidebar />
 </main>
